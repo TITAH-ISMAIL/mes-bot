5da4540686a2d31c7e9defb68586e6a33f5fbdae
ismail abdou <89782333+TITAH-ISMAIL@users.noreply.github.com>
2023-08-05 18:16:41 +0100

    Update index.js
    add webhook

--- index.js
+++ index.js
@@ -6,15 +6,28 @@ const app = express()
 
 app.set("port", process.env.PORT || 8000);
 
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json());
 
-app.get('/',function(req,res){
+app.get('/', function (req, res) {
     res.send('Chat bot web app for messenger')
 })
 
-app.listen(app.get('port'),function(){
+app.get('/webhook', function (req, res) {
+    const PAGE_VERF_TOKEN = 'our chat bot @ app'
+
+    let token = req.query["hub.verify_token"];
+    let challenge = req.query["hub.challenge"];
+
+    if (token === PAGE_VERF_TOKEN) {
+        res.status(200).send(challenge)
+    }else {
+        res.status(403)
+    }
+})
+
+app.listen(app.get('port'), function () {
     console.log('server is running and listentining ' + app.get('port'));
 })
 
